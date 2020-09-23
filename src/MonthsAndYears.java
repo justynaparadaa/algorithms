4d80c94f2324ac17153331fd271f85702d491a1e
Justyna Parada <justyna.paradaa@gmail.com>
2020-09-23 08:33:20 +0200

    Inputting description of two Alg tasks

--- src/MonthsAndYears.java
+++ src/MonthsAndYears.java
@@ -1,4 +1,30 @@
-package PACKAGE_NAME;
+/*
+        Months and Years
+
+        Everybody in Russia uses Gregorian calendar. In this calendar there are 31 days in
+        January, 28 or 29 days in February (depending on whether the year is leap or not), 31 days in
+        March, 30 days in April, 31 days in May, 30 in June, 31 in July, 31 in August, 30 in
+        September, 31 in October, 30 in November, 31 in December.
+
+        A year is leap in one of two cases: either its number is divisible by 4, but not divisible by 100, or
+        is divisible by 400. For example, the following years are leap: 2000, 2004, but
+        years 1900 and 2018 are not leap.
+        In this problem you are given n (1 ≤ n ≤ 24) integers a1, a2, ..., an, and you have to check if these
+        integers could be durations in days of n consecutive months, according to Gregorian calendar.
+        Note that these months could belong to several consecutive years. In other words, check if there
+        is a month in some year, such that its duration is a1 days, duration of the next month is a2 days,
+        and so on.
+
+        Input
+        The first line contains single integer n (1 ≤ n ≤ 24) — the number of integers.
+        The second line contains n integers a1, a2, ..., an (28 ≤ ai ≤ 31) — the numbers you are to check.
+
+        Output
+        If there are several consecutive months that fit the sequence, print "YES" (without quotes).
+        Otherwise, print "NO" (without quotes).
+        You can print each letter in arbitrary case (small or large).'
+*/
 
 public class MonthsAndYears {
+    //TODO
 }
